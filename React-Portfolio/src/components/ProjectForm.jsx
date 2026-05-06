799a786414d47cc16dd70d57e3bb57042e2fdedc
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-06 14:48:37 +0300

    added a return div of a form for title inputs,description and a sumit button

--- React-Portfolio/src/components/ProjectForm.jsx
+++ React-Portfolio/src/components/ProjectForm.jsx
@@ -9,8 +9,42 @@ function ProjectForm({ onAddProject }) {
 
         // validation
         if (!title || !description) return;
-
+        
         onAddProject({ title, description });
 
-    }
+        // clearing inputs after submitting
+        setTitle("");
+        setDescription("");
+
+    };
+
+    return (
+        <div className="card">
+            <h2>Add Project</h2>
+
+            <form onSubmit={handleSubmit}>
+
+                // title input
+                <label>Title</label>
+                <input 
+                   type="text"
+                   value={title}
+                   onChange={(e) => setTitle(e.target.value)} 
+                />
+                // description input
+                <label>Description</label>
+                <textarea 
+                   value={description}
+                   onChange={(e) => setDescription(e.target.value)}
+                
+                />
+                // submit button
+                <button type="submit">Add</button>
+
+            </form>
+
+        </div>
+    )
 }
+
+export default ProjectForm;
