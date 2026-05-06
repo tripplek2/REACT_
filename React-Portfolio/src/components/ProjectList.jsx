f35cd3c10562b720b72bf75b345facebf8b3bc77
Kelvin Korir <kibetkelvin222@gmail.com>
2026-05-06 15:13:09 +0300

    add an if statement to check match search

---
+++ React-Portfolio/src/components/ProjectList.jsx
@@ -0,0 +1,14 @@
+import ProjectCard from "./ProjectCard";
+
+function ProjectList({ projects }) {
+    if (projects.length === 0) {
+        return <p>No projects found</p>;
+    }
+
+    return (
+        <div className="card">
+            
+
+        </div>
+    )
+}
